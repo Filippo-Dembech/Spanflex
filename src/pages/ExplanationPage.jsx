46697be7cb13b3e65df5c8d976518db74ef98223
Filippo Dembech <filippo.dembech@gmail.com>
2025-03-05 15:00:33 +0100

    create: ExplanationPage

---
+++ src/pages/ExplanationPage.jsx
@@ -0,0 +1,9 @@
+import PageLayout from './PageLayout';
+
+export default function ExplanationPage() {
+    return (
+        <PageLayout gobackButton>
+            <div>Explanation Page</div>
+        </PageLayout>
+    )
+}
--- src/pages/SettingsPage.jsx
+++ src/pages/SettingsPage.jsx
@@ -6,32 +6,38 @@ export default function SettingsPage() {
     useUserParameters();
 
   return (
-    <PageLayout gobackButton>
-      <h2>Settings</h2>
-      <label htmlFor="span">Span</label>
-      <input
-        onChange={(e) => setSpan(Number(e.target.value))}
-        type="number"
-        placeholder={`current = ${span}`}
-        name="span"
-        id="span"
-      />
-      <label htmlFor="interval">Interval</label>
-      <input
-        onChange={(e) => setShowSpeed(Number(e.target.value))}
-        type="number"
-        placeholder={`current = ${showSpeed}s`}
-        name="interval"
-        id="interval"
-      />
-      <label htmlFor="numeric">Numeric</label>
-      <input
-        onClick={(e) => setIsNumeric(e.target.checked)}
-        type="checkbox"
-        checked={isNumeric}
-        name="numeric"
-        id="numeric"
-      />
+    <PageLayout gobackButton className="px-8 py-3">
+      <h2 className="text-3xl">Settings</h2>
+      <div className="flex flex-col">
+        <label htmlFor="span">Span</label>
+        <input
+          className="rounded-full bg-white px-5 py-2"
+          onChange={(e) => setSpan(Number(e.target.value))}
+          type="number"
+          placeholder={`current = ${span}`}
+          name="span"
+          id="span"
+        />
+        <label htmlFor="interval">Interval</label>
+        <input
+          className="rounded-full bg-white px-5 py-2"
+          onChange={(e) => setShowSpeed(Number(e.target.value))}
+          type="number"
+          placeholder={`current = ${showSpeed}s`}
+          name="interval"
+          id="interval"
+        />
+        <div>
+          <label htmlFor="numeric">Numeric</label>
+          <input
+            onClick={(e) => setIsNumeric(e.target.checked)}
+            type="checkbox"
+            checked={isNumeric}
+            name="numeric"
+            id="numeric"
+          />
+        </div>
+      </div>
     </PageLayout>
   );
 }
